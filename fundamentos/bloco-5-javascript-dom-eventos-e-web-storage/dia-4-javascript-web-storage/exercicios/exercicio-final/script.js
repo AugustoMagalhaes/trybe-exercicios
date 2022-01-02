6aac55b9c269abd3712ce821afa1559f0c909c0a
AugustoMagalhaes <augustommg@gmail.com>
2022-01-01 23:29:41 -0300

    Adicionando a funçao loadPage que sera inicializada no window.onload, tendo o papel de inicializar os valores armazenados em localStorage assim como os 'valores' dos inputs em #editing-sidebar.

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercicios/exercicio-final/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercicios/exercicio-final/script.js
@@ -1,4 +1,3 @@
-const targetText = document.getElementsByClassName('verse');
 const targetContent = document.getElementById('content');
 const newBgColor = document.getElementById('new-bgcolor');
 const newColor = document.getElementById('new-color');
@@ -8,32 +7,72 @@ const selectFontFamily = document.getElementById('select-fontfamily');
 
 function setBgColor() {
     targetContent.style.backgroundColor = newBgColor.value;
+    localStorage.setItem('background-color', newBgColor.value);
 }
 
 newBgColor.addEventListener('change', setBgColor);
 
 function setColor() {
     targetContent.style.color = newColor.value;
+    localStorage.setItem('color', newColor.value);
 }
 
 newColor.addEventListener('change', setColor);
 
 function setTextSize() {
-    targetContent.style.fontSize = newTextSize.value + "px";
+    targetContent.style.fontSize = newTextSize.value + 'px';
+    localStorage.setItem('font-size', newTextSize.value + 'px');
+    localStorage.setItem('fontSize-value', newTextSize.value);
 }
 
 newTextSize.addEventListener('change', setTextSize);
 
 function setLineHeight() {
     targetContent.style.lineHeight = newLineHeight.value;
+    localStorage.setItem('line-height', newLineHeight.value);
 }
 
 newLineHeight.addEventListener('change', setLineHeight);
 
 function setFontFamily() {
-    const optionFontFamily = selectFontFamily.options[selectFontFamily.selectedIndex];
-    targetContent.style.fontFamily = optionFontFamily.value;
+    const actualFontSelect = selectFontFamily.options;
+    targetContent.style.fontFamily = actualFontSelect[selectFontFamily.selectedIndex].value;
+    localStorage.setItem('font-family', actualFontSelect[selectFontFamily.selectedIndex].value);
+    localStorage.setItem('selectedIndex', actualFontSelect.selectedIndex);
 }
 
 selectFontFamily.addEventListener('change', setFontFamily);
+
+function onloadValue(element, property) {
+    if (localStorage[property] !== undefined) {
+        element.value = localStorage[property];
+        return element.value;
+    } else {
+        element.value = window.getComputedStyle(element, null).getPropertyValue(property);
+    }
+}
+
+function loadPage() {
+    let pageStyle = targetContent.style;
+    pageStyle.backgroundColor = onloadValue(newBgColor, 'background-color');
+    pageStyle.color = onloadValue(newColor, 'color');
+    pageStyle.fontSize = localStorage['font-size'];
+    newTextSize.value = parseFloat(localStorage['fontSize-value']);
+    pageStyle.lineHeight = onloadValue(newLineHeight, 'line-height')
+    pageStyle.fontFamily = localStorage['font-family'];
+    selectFontFamily.options.selectedIndex = localStorage['selectedIndex'];
+}
+
+window.onload = function () {
+    loadPage();
+}
+
+function lag() {
+    console.log('newBgColor', newBgColor.value);
+    console.log('newColor', newColor.value);
+    console.log('newTextSize', newTextSize.value);
+    console.log('tg font-size', targetContent.style.fontSize);
+    console.log('newLineHeight', newLineHeight.value);
+    console.log('font family', selectFontFamily.options[selectFontFamily.selectedIndex].value);
+}
 
