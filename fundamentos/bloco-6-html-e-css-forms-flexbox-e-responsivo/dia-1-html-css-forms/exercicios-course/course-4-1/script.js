5ef126679f3ddbb42ff12b4eaec8a2e7b2fa6556
AugustoMagalhaes <augustommg@gmail.com>
2022-01-05 02:11:18 -0300

    Adicionando a funcao 'lazyCheckbox' que previne o comportamento padrao do input '#input-checkbox'.

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicios-course/course-4-1/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicios-course/course-4-1/script.js
@@ -5,4 +5,11 @@ const HREF_LINK = document.querySelector("#href");
 function anchorLink(event) {
   event.preventDefault();  
 }
-HREF_LINK.addEventListener('click', anchorLink)
+HREF_LINK.addEventListener('click', anchorLink);
+
+function lazyCheckbox(event) {
+  event.preventDefault();
+}
+
+INPUT_CHECKBOX.addEventListener('click', lazyCheckbox);
+
