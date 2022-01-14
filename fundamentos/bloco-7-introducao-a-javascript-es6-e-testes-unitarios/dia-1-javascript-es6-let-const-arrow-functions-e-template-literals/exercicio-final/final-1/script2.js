ab33d4f2e29292b390586d8452a52752dc866049
AugustoMagalhaes <augustommg@gmail.com>
2022-01-14 15:31:49 -0300

    Corrigindo o nome da array dentro da template literal.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio-final/final-1/script2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio-final/final-1/script2.js
@@ -1,5 +1,5 @@
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 const sortedArray = oddsAndEvens.sort( (a,b)  => a - b);
-const finalMessage = `Os números ${[...oddsAndEvens]} se encontram ordenados de forma crescente!`
+const finalMessage = `Os números ${[...sortedArray]} se encontram ordenados de forma crescente!`
 
 console.log(finalMessage); 
