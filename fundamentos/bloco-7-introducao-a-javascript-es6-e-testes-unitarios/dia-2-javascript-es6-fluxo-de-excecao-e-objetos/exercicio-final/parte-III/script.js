fa40c772e5f6da90d68861a39972453697c5a818
AugustoMagalhaes <augustommg@gmail.com>
2022-01-13 21:11:51 -0300

    Criando a funçao 'setProp' que permite criaçao de chaves e valores em objetos genericos; usando 'setProp' para criar o turno da noite em 'lesson2'.

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/exercicio-final/parte-III/script.js
@@ -0,0 +1,28 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const setProp = (obj, key, value) => {
+  obj[key] = value;
+  console.log(obj.key)
+  return obj[key];
+}
+
+setProp(lesson2, 'turno', 'noite');
+console.log(lesson2)
