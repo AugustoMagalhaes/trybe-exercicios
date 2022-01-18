c4f21891a2b76fca8fd711d00f0ac0d9c75f03e2
AugustoMagalhaes <augustommg@gmail.com>
2022-01-18 16:23:13 -0300

    Testando se o retorno das funçoes encode e decode nao sao diferentes do que deveriam ser.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicio-final/encodeDecode.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicio-final/encodeDecode.test.js
@@ -11,7 +11,7 @@ describe('Testando as funçoes encode e decode', () => {
     });
 });
 
-describe('Testando se a saida de encode equivale a entrada de decode', () => {
+describe('Testando se a saida de encode equivale a entrada de decode e vice versa', () => {
     it('Testa se as vogais a,e,i,o,u sao convertidas em 1,2,3,4,5 pela funçao encode', () => {
         expect(encode('aeiou')).toMatch('12345');
     });
@@ -19,4 +19,22 @@ describe('Testando se a saida de encode equivale a entrada de decode', () => {
     it('Testa se a string "1,2,3,4,5" é convertida em "aeiou" por decode', () => {
         expect(decode('12345')).toMatch('aeiou');
     });
+});
+
+describe('Testa se as saidas sao unicas', () => {
+    it('Testa se as vogais a,e,i,o,u nao sao convertidas em outros numeros pela funçao encode', () => {
+        expect(encode('aeiou')).not.toMatch('13245');
+        expect(encode('aeiou')).not.toMatch('21345');
+        expect(encode('aeiou')).not.toMatch('32145');
+        expect(encode('aeiou')).not.toMatch('54123');
+        expect(encode('aeiou')).not.toMatch('42135');
+    });
+
+    it('Testa se os numeros 1,2,3,4,5 nao sao convertidos em outras vogais pela funçao decode', () => {
+        expect(decode('12345')).not.toMatch('aieou');
+        expect(decode('12345')).not.toMatch('iaeou');
+        expect(decode('12345')).not.toMatch('aueoi');
+        expect(decode('12345')).not.toMatch('aaeou');
+        expect(decode('12345')).not.toMatch('eaoou');
+    })
 })
