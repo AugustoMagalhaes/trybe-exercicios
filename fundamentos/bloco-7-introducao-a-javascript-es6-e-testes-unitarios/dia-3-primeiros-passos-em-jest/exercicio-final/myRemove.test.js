0480b735a6cb88a830f45896db0b28f37aeee636
AugustoMagalhaes <augustommg@gmail.com>
2022-01-18 15:02:42 -0300

    Checa se myRemove([1,2,3,4], 3) retira o elemento '3' e nao devolve a array original.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicio-final/myRemove.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicio-final/myRemove.test.js
@@ -2,4 +2,8 @@ const myRemove = require('./myRemove');
 
 test('Checa se myRemove([1,2,3,4], 3) retorna o array esperado', () => {
     expect(myRemove([1,2,3,4], 3)).toEqual([1,2,4]);
+});
+
+test('Checa se myRemove([1,2,3,4], 3) retira o elemento "3" e nao devolve a array original', () => {
+    expect(myRemove([1,2,3,4], 3)).not.toEqual([1,2,3,4]);
 })
