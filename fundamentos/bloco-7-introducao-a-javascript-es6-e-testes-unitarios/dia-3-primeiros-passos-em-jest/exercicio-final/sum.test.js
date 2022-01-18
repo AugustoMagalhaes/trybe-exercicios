0299493c1a8d9fabe291e3d41f5c9e704b315b5c
AugustoMagalhaes <augustommg@gmail.com>
2022-01-18 14:12:11 -0300

    Criando o segundo teste da primeira parte, que testa se a soma de 0 e 0 e igual a 0.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicio-final/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicio-final/sum.test.js
@@ -2,7 +2,11 @@ const { expect } = require('@jest/globals');
 const sum = require('./sum.js');
 
 describe('Testando a funçao sum', () => {
-    test('Testando se sum(4,5) é igual a 9', () => {
+    it('Testando se sum(4,5) é igual a 9', () => {
         expect(sum(4, 5)).toBe(9);
     });
+
+    it('Testando se sum(0,0) é igual a 0', () => {
+        expect(sum(0,0)).toBe(0);
+    })
 })
