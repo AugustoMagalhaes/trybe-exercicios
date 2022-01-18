2b3647da9a8778423198bd06415373c15a343917
AugustoMagalhaes <augustommg@gmail.com>
2022-01-18 14:18:26 -0300

    Testando se a funçao 'sum' lança um erro quando um dos parametros e do tipo string.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicio-final/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicio-final/sum.test.js
@@ -8,5 +8,11 @@ describe('Testando a funçao sum', () => {
 
     it('Testando se sum(0,0) é igual a 0', () => {
         expect(sum(0,0)).toBe(0);
+    });
+
+    it('Testando se um erro é lançado quando um dos argumentos é do tipo string', () => {
+        expect( () => {
+            sum(4,'5');
+        }).toThrow();        
     })
 })
