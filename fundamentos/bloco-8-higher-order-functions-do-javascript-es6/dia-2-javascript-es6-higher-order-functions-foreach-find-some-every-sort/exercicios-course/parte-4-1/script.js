5a0b108e202055a60ea36dd38052b31456e68206
AugustoMagalhaes <augustommg@gmail.com>
2022-01-20 01:26:59 -0300

    Construindo a funçao que retorna 'true' se o elemento tem 5 letras e depois a usando como 'callback' da funçao 'find' na array 'names'.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios-course/parte-4-1/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios-course/parte-4-1/script.js
@@ -6,3 +6,12 @@ const findDivisibleBy3And5 = (element) => {
 
 console.log(numbers.find(findDivisibleBy3And5));
 
+// exercicio 2
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = (element) => {
+  return (element.length === 5);
+}
+
+console.log(names.find(findNameWithFiveLetters));
