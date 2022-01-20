3ad6a4e699fcb8f4040c063e77bde7a40364a6dc
AugustoMagalhaes <augustommg@gmail.com>
2022-01-20 01:52:26 -0300

    Adicionando resoluçao dos exercicios da seçao 5 do dia 8.2 do 'course'.

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios-course/parte-5-1/script.js
@@ -0,0 +1,23 @@
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  //Adicione seu código aqui
+  return arr.some((element) => name === element);
+}
+
+console.log(hasName(names, 'Ana'));
+
+const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+  ];
+  
+  const verifyAges = (arr, minimumAge) => {
+    //Adicione seu código aqui
+    return Object.values(arr).every( (element) => element >= minimumAge);
+  }
+  
+  console.log(verifyAges(people, 18));
