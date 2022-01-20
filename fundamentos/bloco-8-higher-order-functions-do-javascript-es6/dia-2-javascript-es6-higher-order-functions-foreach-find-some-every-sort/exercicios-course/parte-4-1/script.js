3552e93f9f6177847d6514e8439232c078494a5d
AugustoMagalhaes <augustommg@gmail.com>
2022-01-20 01:45:11 -0300

    Resolvendo terceiro exercicio da 4a seçao do course, encontrando o nome da musica que tem um 'id' dado numa array de objetos.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios-course/parte-4-1/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios-course/parte-4-1/script.js
@@ -15,3 +15,17 @@ const findNameWithFiveLetters = (element) => {
 }
 
 console.log(names.find(findNameWithFiveLetters));
+
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+]
+
+function findMusic(song) {
+  // Adicione seu código aqui  
+  return (song.id === '31031685') ? true : false;
+}
+
+console.log(musicas.find(findMusic).title);
+
