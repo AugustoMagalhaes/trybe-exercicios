71dd4789ae44e7e5fab2312c48c935c3be2c859c
AugustoMagalhaes <augustommg@gmail.com>
2022-01-28 18:48:20 -0300

    Exercicio 1 da parte 3 do conteudo do course do dia 9.1.

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicios-course/parte-2-1.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicios-course/parte-2-1.js
@@ -3,14 +3,14 @@ const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName
 const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
 
 // Definição da função getUser
-const getUser = (param) => {
+const getUser = (callback) => {
   const userToReturn = {
     firstName: "Ivan",
     lastName: "Ivanovich",
     nationality: "Russian"
   };
   // Retornamos nosso parâmetro, que será uma função (callback)
-  return param(userToReturn);
+  return callback(userToReturn);
 };
 
 // Chamada/execução da função getUser, que vai receber como parâmetro nossa função userFullName.
