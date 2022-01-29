d2966d7c8f037638926b7666c96e3773d92b07e1
AugustoMagalhaes <augustommg@gmail.com>
2022-01-29 02:54:10 -0300

    Resoluçao dos exercicios 3 e 4 da seçao final de exercicios do bloco 9.1

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicio-final/exercise3.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicio-final/exercise3.js
@@ -1,12 +1,14 @@
 const getPlanet = () => {
-  const mars = {
-    name: "Mars",
-    distanceFromSun: {
-      value: 227900000,
-      measurementUnit: "kilometers",
-    },
-  };
-  console.log("Returned planet: ", mars);
+  setTimeout(() => {
+    const mars = {
+      name: 'Mars',
+      distanceFromSun: {
+        value: 227900000,
+        measurementUnit: 'kilometers',
+      },
+    };    
+    console.log('Returned planet: ', mars);
+  }, 4000);
 };
 
 getPlanet(); // imprime Marte depois de 4 segundos
