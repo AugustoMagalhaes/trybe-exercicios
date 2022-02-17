de3b690e8bd438504767589fd2502bc9fb29774f
AugustoMagalhaes <augustommg@gmail.com>
2022-02-17 15:56:07 -0300

    Alterando o app de 'fixation-exercises-10-2' de modo a criar os elementos dinamicamente; adicionando dinamicamente as keys de cada Componente criado.

--- fundamentos/bloco-10-introducao-a-react/dia-2-componentes-react/exercicios-course/fixation-exercises-10-2/src/UserProfile.js
+++ fundamentos/bloco-10-introducao-a-react/dia-2-componentes-react/exercicios-course/fixation-exercises-10-2/src/UserProfile.js
@@ -3,11 +3,12 @@ import Image from './Image';
 
 class UserProfile extends React.Component {
   render() {
+    const altTextUser = `${this.props.user.name}\'s picture`
     return (
       <div>
         <p> {this.props.user.name} </p>
         <p> {this.props.user.email} </p>
-        <Image source={this.props.user.avatar} alternativeText="User avatar" />
+        <Image source={this.props.user.avatar} alternativeText={altTextUser}/>
       </div>
     );
   }
