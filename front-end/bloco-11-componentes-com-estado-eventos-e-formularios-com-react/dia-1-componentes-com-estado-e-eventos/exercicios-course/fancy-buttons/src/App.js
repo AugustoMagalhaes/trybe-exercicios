d67deef8b984261f5f515140c6b8d52d8fa56671
AugustoMagalhaes <augustommg@gmail.com>
2022-02-21 18:23:53 -0300

    Adicionando o 'constructor' ao 'App' para permitir o bind das tres funçoes.

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercicios-course/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercicios-course/fancy-buttons/src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import './App.css';
 
-function handleClick() {
-  console.log('Clicou');
-}
-function handleClick2() {
-  console.log('Agora no segundo botão!');
-}
-function handleClick3() {
-  console.log('Último botão :)');
-}
 class App extends React.Component {
-    render() {
-      return (
-        <>
-        <button onClick={handleClick}>Botão 1</button>
-        <button onClick={handleClick2}>Botão 2</button>
-        <button onClick={handleClick3}>Botão 3</button>
-        </>
-      )
-    }
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClick2 = this.handleClick2.bind(this);
+    this.handleClick3 = this.handleClick3.bind(this);
+  }
+
+  handleClick() {
+    console.log('Clicou');
+  }
+  handleClick2() {
+    console.log('Agora no segundo botão!');
+  }
+  handleClick3() {
+    console.log('Último botão :)');
+  }
+
+  render() {
+    return (
+      <>
+        <button onClick={this.handleClick}>Botão 1</button>
+        <button onClick={this.handleClick2}>Botão 2</button>
+        <button onClick={this.handleClick3}>Botão 3</button>
+      </>
+    );
+  }
 }
 
 export default App;
