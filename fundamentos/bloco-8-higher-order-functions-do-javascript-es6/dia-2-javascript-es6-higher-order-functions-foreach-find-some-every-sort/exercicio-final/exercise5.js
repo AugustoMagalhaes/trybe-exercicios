10be86d0b8efed7d4b8b1212715959ac48383ace
AugustoMagalhaes <86219529+AugustoMagalhaes@users.noreply.github.com>
2022-02-22 12:57:03 -0300

    Delete fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort directory

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio-final/exercise5.js
+++
@@ -1,9 +0,0 @@
-const books = require('./template.js');
-
-function everyoneWasBornOnSecXX(element) {
-  // escreva seu código aqui
-  const year = element.author.birthYear;
-  return (year > 1900 && year <= 2000);
-}
-
-console.log(books.every(everyoneWasBornOnSecXX));
--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio-final/exercise6.js
+++
@@ -1,9 +0,0 @@
-const books = require('./template.js');
-
-function someBookWasReleaseOnThe80s(element) {
-  // escreva seu código aqui
-  const release = element.releaseYear;
-  return (release >= 1980 && release < 1990);
-}
-
-console.log(books.some(someBookWasReleaseOnThe80s));
