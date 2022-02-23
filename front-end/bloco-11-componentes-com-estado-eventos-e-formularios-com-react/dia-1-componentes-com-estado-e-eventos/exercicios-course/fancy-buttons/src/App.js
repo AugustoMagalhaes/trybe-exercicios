aee6d3b44ebb8cf92ca5d354cd1db8750ef69670
AugustoMagalhaes <augustommg@gmail.com>
2022-02-23 00:59:01 -0300

    Adicionando alteraçao dinamica da cor do texto dos botoes quando a contagem de cliques e' par.

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercicios-course/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercicios-course/fancy-buttons/src/App.js
@@ -5,29 +5,58 @@ class App extends React.Component {
   constructor() {
     super()
     this.handleClick = this.handleClick.bind(this);
+    this.changeColor = this.changeColor.bind(this);
     this.state = {
       firstBtnClicks: 0,
       secBtnClicks: 0,
       thirdBtnClicks: 0,
+      firstBtnClicksColor: 'black',
+      secBtnClicksColor: 'black',
+      thirdBtnClicksColor: 'black',
     }
   }
 
+  changeColor(element) {
+    return (this.state[element] % 2 === 0) 
+      ? 'black' : 'white';
+  }
+
   handleClick(event) {
-    const { name, textContent } = event.target;
+    const { name } = event.target;
     this.setState((previousState, _props) => ({
       [name]: previousState[name] + 1,
-    } ))
+    }))
+  }
+
+  componentDidUpdate(_prevProps, prevState) {
+    const prevKeys = Object.keys(prevState);
+    const diff = prevKeys.find((elem) => {
+      return prevState[elem] !== this.state[elem]
+    })
+    if (diff) {
+    this.setState({
+      [diff+'Color']: this.changeColor(diff),
+    })
+    }
   }
 
   render() {
+    const { state: {
+      firstBtnClicksColor, secBtnClicksColor, thirdBtnClicksColor,
+      firstBtnClicks, secBtnClicks, thirdBtnClicks,
+    }, handleClick} = this;
+
     return (
       <>
-        <button onClick={this.handleClick} name="firstBtnClicks">Botão 1
-        <br/>Cliques: {this.state.firstBtnClicks}</button>
-        <button onClick={this.handleClick} name="secBtnClicks">Botão 2
-        <br/>Cliques: {this.state.secBtnClicks}</button>
-        <button onClick={this.handleClick} name="thirdBtnClicks">Botão 3
-          <br/>Cliques: {this.state.thirdBtnClicks}</button>
+        <button onClick={handleClick} style={{color: firstBtnClicksColor}} 
+          name="firstBtnClicks">Botão 1
+          <br/>Cliques: {firstBtnClicks}</button>
+        <button onClick={handleClick} style={{color: secBtnClicksColor}} 
+          name="secBtnClicks">Botão 2
+          <br/>Cliques: {secBtnClicks}</button>
+        <button onClick={handleClick} style={{color: thirdBtnClicksColor}}
+          name="thirdBtnClicks">Botão 3
+          <br/>Cliques: {thirdBtnClicks}</button>
       </>
     );
   }
