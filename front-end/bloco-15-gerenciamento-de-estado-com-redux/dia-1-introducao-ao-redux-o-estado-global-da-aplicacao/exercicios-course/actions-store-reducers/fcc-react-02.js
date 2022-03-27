2428a44505cca13161f6a38574eb943e044b2f57
AugustoMagalhaes <augustommg@gmail.com>
2022-03-27 05:39:18 -0300

    Exercicio 2 FCC - pegando o state a partir da store.

---
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/exercicios-course/actions-store-reducers/fcc-react-02.js
@@ -0,0 +1,10 @@
+const Redux = require('redux');
+
+const store = Redux.createStore(
+  (state = 5) => state
+);
+
+// Change code below this line
+const currentState = store.getState();
+
+console.log(currentState)
