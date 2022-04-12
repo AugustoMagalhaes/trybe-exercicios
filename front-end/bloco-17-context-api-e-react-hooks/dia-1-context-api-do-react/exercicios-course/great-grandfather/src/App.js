e958aa1a402b0b13c514e34eb73fe5d3ab3044ee
AugustoMagalhaes <augustommg@gmail.com>
2022-04-12 15:59:11 -0300

    Passando callback com prop drilling.

--- front-end/bloco-17-context-api-e-react-hooks/dia-1-context-api-do-react/exercicios-course/great-grandfather/src/App.js
+++ front-end/bloco-17-context-api-e-react-hooks/dia-1-context-api-do-react/exercicios-course/great-grandfather/src/App.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react';
 
 class GreatGrandfather extends Component {
-  state = {
-    inheritance: 1000000,
+  constructor(props) {
+    super(props);
+    this.state = {
+      inheritance: 1000000,
+    }
+    this.spendInheritance = this.spendInheritance.bind(this);
+  }
+
+  spendInheritance() {
+    this.setState((prevState) => (
+      { inheritance: prevState.inheritance - 1000 }
+    ));
   }
 
   render() {
     return (
-      <Grandmother inheritance={this.state.inheritance} />
+      <Grandmother
+        inheritance={this.state.inheritance}
+        spendInheritance={this.spendInheritance}
+      />
     );
   }
 }
 
 function Grandmother(props) {
   return (
-    <Father inheritance={props.inheritance} />
+    <Father
+      inheritance={props.inheritance}
+      spendInheritance={props.spendInheritance}
+    />
   );
 }
 
 function Father(props) {
   return (
-    <Daughter inheritance={props.inheritance} />
+    <Daughter
+      inheritance={props.inheritance}
+      spendInheritance={props.spendInheritance}
+    />
   );
 }
 
@@ -28,8 +47,11 @@ function Daughter(props) {
   return (
     <div>
       <span>
-        {`Tenho uma herança de R$ ${props.inheritance} que recebi do meu bisavô`}
+        `Tenho uma herança de R$ ${props.inheritance} que recebi do meu bisavô`
       </span>
+      <button type="button" onClick={props.spendInheritance}>
+        Gastar Herança!
+      </button>
     </div>
   );
 }
