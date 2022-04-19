821567b1e90e875fbab09de63ff75a001d3ec562
Gabriel Dalseco <gabriel_santos96@outlook.com>
2022-04-19 14:38:40 -0300

    fix: muda config para usar variavel de ambiente

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: 'hospital_control',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+  test: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: 'hospital_control',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: 'hospital_control',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+};
