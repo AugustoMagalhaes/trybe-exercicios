48d7172722b677366f86cdfd640c615f15741309
AugustoMagalhaes <augustommg@gmail.com>
2022-06-10 17:56:25 -0300

    implementa parametro na url para pesquisa de receita especifica por id.

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicios-course/api-sample/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicios-course/api-sample/index.js
@@ -30,7 +30,16 @@ app.get('/recipes', function (req, res) {
 
 app.get('/drinks', function (req, res) {
   res.json(orderedDrinks);
-})
+});
+
+app.get('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const recipe = recipes.find((r) => r.id === Number(id));
+
+  if (!recipe) return res.status(404).json({ message: 'Recipe not found!'});
+
+  res.status(200).json(recipe);
+});
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
