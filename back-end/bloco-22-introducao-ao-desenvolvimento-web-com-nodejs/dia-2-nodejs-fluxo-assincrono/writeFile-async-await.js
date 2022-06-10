9e10a7321644e9f29a01129815c55332bacbb30f
AugustoMagalhaes <augustommg@gmail.com>
2022-06-09 23:52:29 -0300

    sinc repo

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/writeFile-async-await.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/writeFile-async-await.js
@@ -2,7 +2,7 @@ const fs = require('fs').promises;
 
 async function main() {
   try {
-    await fs.writeFile('./meu-arquivo.txt', 'Meu textão2');
+    await fs.writeFile('./meu-arquivo.txt', 'Meu textão3');
     console.log('Arquivo escrito com sucesso!');
   } catch (err) {
     console.error(`Erro ao escrever o arquivo: ${err.message}`);
