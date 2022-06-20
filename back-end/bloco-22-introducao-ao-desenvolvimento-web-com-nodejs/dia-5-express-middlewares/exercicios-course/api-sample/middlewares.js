bbb400d8b9d4d0f75c7ab30667f0fbc48d50125a
AugustoMagalhaes <augustommg@gmail.com>
2022-06-20 17:23:16 -0300

    chore: altera middleware de autenticaçao pra armazenar valor de 'user'.

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/exercicios-course/api-sample/middlewares.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/exercicios-course/api-sample/middlewares.js
@@ -12,11 +12,12 @@ function validatePrice(req, res, next) {
   next();
 };
 
-/* auth-middleware.js */
-const validUser = {
-  username: 'MestreCuca',
-  password: 'MinhaSenhaSuperSeguraSqn'
-};
+const validUsers = [
+  { username: 'MestreCuca', password: 'MinhaSenhaSuperSeguraSqn' },
+  { username: 'McRonald', password: 'Senha123Mudar' },
+  { username: 'Burger Queen', password: 'Senha123Mudar' },
+  { username: 'UpWay', password: 'Senha123Mudar' },
+];
 
 const authMiddleware = (req, res, next) => {
   const { username, password } = req.headers;
@@ -28,6 +29,16 @@ const authMiddleware = (req, res, next) => {
   if (username !== validUser.username || password !== validUser.password) {
     return res.status(401).json({ message: 'Invalid credentials!' });
   }
+
+  const foundUser = validUsers.find((user) => user.username === username);
+
+  if (!foundUser) return res.status(401).json({ message: 'Invalid credentials!' });
+
+  if (!(username === foundUser.username  && password === foundUser.password)) {
+    return res.status(401).json({ message: 'Invalid credentials!' });
+  }
+
+  req.user = foundUser;
 
   next();
 };
