c09083201fabb576eb80a0e4a0da76d8fa1481b6
AugustoMagalhaes <augustommg@gmail.com>
2022-06-23 16:06:18 -0300

    fix: corrige erro de variavel sem 'await'.

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios-course/model_example/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios-course/model_example/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 
+const Author = require('./model/Author');
+
 const app = express();
+
+app.get('/', async (_req, res) => {
+  const authors = await Author.getAll();
+
+  res.status(200).json(authors);
+});
 
 const PORT = process.env.PORT || 3000;
 
