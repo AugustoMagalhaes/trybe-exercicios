0136cb7200a7f6c1c221747dcc07df4811a9fa67
AugustoMagalhaes <augustommg@gmail.com>
2022-06-23 16:17:50 -0300

    feat: adiciona table 'books' a database.

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios-course/model_example/database.sql
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios-course/model_example/database.sql
@@ -20,3 +20,21 @@ VALUES ('George','R. R.','Martin','1948-09-20','norte-americano'),
 	('Isaac',NULL,'Asimov','1920-01-20','russo-americano'),
 	('Frank',NULL,'Herbert','1920-02-11','norte-americano'),
 	('Júlio',NULL,'Verne','1905-03-24','francês');
+    
+CREATE TABLE books (
+	id INT NOT NULL AUTO_INCREMENT,
+	title VARCHAR(90) NOT NULL,
+	author_id INT(11) NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY (author_id) REFERENCES authors (id)
+);
+
+INSERT INTO books (title, author_id)
+VALUES
+	('A Game of Thrones', 1),
+	('A Clash of Kings', 1),
+	('A Storm of Swords', 1),
+	('The Lord of The Rings - The Fellowship of the Ring', 2),
+	('The Lord of The Rings - The Two Towers', 2),
+	('The Lord of The Rings - The Return of The King', 2),
+	('Foundation', 3);
