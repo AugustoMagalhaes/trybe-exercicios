4831b817e9db88221e2388a29d7752268852cc4e
AugustoMagalhaes <augustommg@gmail.com>
2022-06-23 15:02:27 -0300

    feat: adiciona funçao serialize para formatar o retorno de getAll.

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios-course/model_example/model/Author.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios-course/model_example/model/Author.js
@@ -1,12 +1,20 @@
 const connection = require('./connection');
 
+// Converte o nome dos campos de snake_case para camelCase
+const serialize = (authorData) => ({
+	id: authorData.id,
+	firstName: authorData.first_name,
+	middleName: authorData.middle_name,
+	lastName: authorData.last_name
+});
+
 // Busca todas as pessoas autoras do banco.
 
 const getAll = async () => {
 	const [authors] = await connection.execute(
 		'SELECT id, first_name, middle_name, last_name FROM model_example.authors;',
 	);
-	return authors;
+	return authors.map(serialize);
 };
 
 module.exports = {
