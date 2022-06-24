94726009db1f3b98502b491df99441dc231cce68
AugustoMagalhaes <augustommg@gmail.com>
2022-06-24 01:18:51 -0300

    feat: implementa rota POST '/authors'.

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios-course/model_example/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios-course/model_example/index.js
@@ -37,7 +37,19 @@ app.get('/books/:id', async (req, res) => {
   if (!booksByAuthorId) return res.status(404).json({message: 'Livro não encontrado'});
 
   res.status(200).json(booksByAuthorId);
-})
+});
+
+app.post('/authors', async (req, res) => {
+	const { first_name, middle_name, last_name } = req.body;
+
+	if (!Author.isValid(first_name, middle_name, last_name)) {
+		return res.status(400).json({ message: 'Dados inválidos' });
+	}
+
+	await Author.create(first_name, middle_name, last_name);
+
+	res.status(201).json({ message: 'Autor criado com sucesso! '});
+});
 
 const PORT = process.env.PORT || 3000;
 
