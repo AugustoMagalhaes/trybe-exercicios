d80c4be0c6bf19363afe6528e68a2a9ee15c0542
AugustoMagalhaes <augustommg@gmail.com>
2022-06-27 14:01:09 -0300

    feat: altera estrutura do service e cria findByName em models.

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios-course/hello-msc/models/Authors.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios-course/hello-msc/models/Authors.js
@@ -52,7 +52,6 @@ const findById = async (id) => {
   return serialize(authorData)[0];
 };
 
-
 const createAuthor = async (firstName, middleName, lastName) => {
   const [author] = await connection.execute(
     'INSERT INTO model_example.authors (first_name, middle_name, last_name) VALUES (?, ?, ?)',
@@ -61,9 +60,32 @@ const createAuthor = async (firstName, middleName, lastName) => {
   return [getNewAuthor({ id: author.insertId, firstName, middleName, lastName })];
 };
 
+const findByName = async (firstName, middleName, lastName) => {
+  // Determinamos se devemos buscar com ou sem o nome do meio
+  let query = `
+    SELECT id, first_name, middle_name, last_name 
+    FROM model_example.authors
+  `;
+
+  if (middleName) {
+    query += 'WHERE first_name = ? AND middle_name = ? AND last_name = ?';
+  } else {
+    query += 'WHERE first_name = ? AND last_name = ?';
+  }
+
+  const params = middleName ? [firstName, middleName, lastName] : [firstName, lastName];
+
+  const [authorData] = await connection.execute(query, params);
+
+  if (authorData.length === 0) return null;
+
+  return serialize(authorData);
+};
+
 module.exports = {
   getAll,
   findById,
   isValid,
   createAuthor,
+  findByName,
 };
--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios-course/hello-msc/services/Authors.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios-course/hello-msc/services/Authors.js
@@ -1,60 +1,14 @@
-const Author = require('../models/Authors');
+const Author = require('../models/Author');
 
-const getNewAuthor = (authorData) => {
-  const { id, firstName, middleName, lastName } = authorData;
+const getAll = async () => Author.getAll();
 
-  const fullName = [firstName, middleName, lastName]
-    .filter((name) => name)
-    .join(' ');
+const findById = async (id) => Author.findById(id);
 
-  return {
-    id,
-    firstName,
-    middleName,
-    lastName,
-    name: fullName,
-  };
-};
-
-const isValid = (firstName, middleName, lastName) => {
-  if (!firstName || typeof firstName !== 'string') return false;
-  if (!lastName || typeof lastName !== 'string') return false;
-  if (middleName && typeof middleName !== 'string') return false;
-
-  return true;
-};
-
-const getAll = async () => {
-  const authors = await Author.getAll();
-
-  return authors.map(getNewAuthor);
-};
-
-const findById = async (id) => {
-  const author = await Author.findById(id);
-
-  if (!author) return null;
-
-  return getNewAuthor(author);
-};
-
-const createAuthor = async (firstName, middleName, lastName) => {
-  const validAuthor = isValid(firstName, middleName, lastName);
-
-  if (!validAuthor) return false;
-
-  const [author] = await Author.createAuthor(firstName, middleName, lastName);
-
-  return getNewAuthor({
-    id: author.id,
-    firstName,
-    middleName,
-    lastName,
-  });
-};
+const createAuthor = async (firstName, middleName, lastName) =>
+  Author.createAuthor(firstName, middleName, lastName);
 
 module.exports = {
   getAll,
   findById,
-  createAuthor
+  createAuthor,
 };
