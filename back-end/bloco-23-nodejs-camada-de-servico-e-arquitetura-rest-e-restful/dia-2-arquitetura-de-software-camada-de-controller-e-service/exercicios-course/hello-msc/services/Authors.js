bf63289dd8f90fc2c28e1a732e08e74d7b861445
AugustoMagalhaes <augustommg@gmail.com>
2022-06-27 14:02:13 -0300

    feat: altera creteAuthor em services pra nao criar usuario com nome completo repetido.

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios-course/hello-msc/services/Authors.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios-course/hello-msc/services/Authors.js
@@ -4,8 +4,20 @@ const getAll = async () => Author.getAll();
 
 const findById = async (id) => Author.findById(id);
 
-const createAuthor = async (firstName, middleName, lastName) =>
-  Author.createAuthor(firstName, middleName, lastName);
+const createAuthor = async (firstName, middleName, lastName) => {
+  const existingAuthor = await Author.findByName(firstName, middleName, lastName);
+
+  if (existingAuthor) {
+    return {
+      error: {
+        code: 'alreadyExists',
+        message: 'Uma pessoa autora já existe com esse nome completo',
+      },
+    };
+  }
+
+  return Author.createAuthor(firstName, middleName, lastName);
+};
 
 module.exports = {
   getAll,
