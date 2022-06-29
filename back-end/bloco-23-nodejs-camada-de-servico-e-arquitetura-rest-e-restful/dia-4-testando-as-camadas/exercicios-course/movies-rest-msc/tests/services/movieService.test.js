af9827aca35e3992eed04fe36631f042029928d5
AugustoMagalhaes <augustommg@gmail.com>
2022-06-29 04:41:41 -0300

    test: implementa teste de movieService.

---
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-4-testando-as-camadas/exercicios-course/movies-rest-msc/tests/services/movieService.test.js
@@ -0,0 +1,45 @@
+const { expect } = require('chai');
+
+const MoviesService = {
+  create: () => {},
+};
+
+describe('Insere um novo filme no BD', () => {
+  describe('quando o payload informado não é válido', () => {
+    const payloadMovie = {};
+
+    it('retorna um boolean', async () => {
+      const response = await MoviesService.create(payloadMovie);
+
+      expect(response).to.be.a('boolean');
+    });
+
+    it('o boolean contém "false"', async () => {
+      const response = await MoviesService.create(payloadMovie);
+
+      expect(response).to.be.equal(false);
+    });
+
+  });
+
+  describe('quando é inserido com sucesso', () => {
+    const payloadMovie = {
+      title: 'Example Movie',
+      directedBy: 'Jane Dow',
+      releaseYear: 1999,
+    };
+
+    it('retorna um objeto', async () => {
+      const response = await MoviesService.create(payloadMovie);
+
+      expect(response).to.be.a('object');
+    });
+
+    it('tal objeto possui o "id" do novo filme inserido', async () => {
+      const response = await MoviesService.create(payloadMovie);
+
+      expect(response).to.have.a.property('id');
+    });
+
+  });
+});
