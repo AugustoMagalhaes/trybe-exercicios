36d72b0119b82475e7b5aac5b6395bc6647143fa
AugustoMagalhaes <augustommg@gmail.com>
2022-07-19 16:44:45 -0300

    feat: adiciona rota GET '/patients'.

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const bodyParser = require('body-parser');
 
 const express = require('express');
-
+const patientController = require('./controllers/patientController');
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const PORT = 3000;
 
+app.get('/patients', patientController.findPatients);
+
 app.listen(PORT, () => {
-  console.log(`Port: ${PORT}`);
+	console.log(`Port: ${PORT}`);
 });
