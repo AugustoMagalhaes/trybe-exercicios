8e4f3129fa041efd727e9a37213aa05b94ff5d72
AugustoMagalhaes <augustommg@gmail.com>
2022-07-20 03:40:35 -0300

    feat: adiciona jwt ao login.

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-3-jwt-json-web-token/exercicios-course/node-jwt-base-project/controllers/login.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-3-jwt-json-web-token/exercicios-course/node-jwt-base-project/controllers/login.js
@@ -1,35 +1,39 @@
 const { User } = require('../models');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
+const secret = process.env.MY_SECRET;
 const validateBody = (body, res) => {
-  const { username, password } = body;
+	const { username, password } = body;
 
-  if (!username || !password) {
-    res
-      .status(401)
-      .json({ message: 'É necessário usuário e senha para fazer login' });
-    return false;
-  }
+	if (!username || !password) {
+		res.status(401).json({ message: 'É necessário usuário e senha para fazer login' });
+		return false;
+	}
 
-  return true;
+	return true;
 };
 
 module.exports = async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    
-    if (!validateBody(req.body, res)) return;
+	try {
+		const { username, password } = req.body;
 
-    const user = await User.findOne({ where: { username } });
-  
-    if (!user || user.password !== password) {
-      return res
-        .status(401)
-        .json({ message: 'Usuário não existe ou senha inválida' });
-    }
-    return res.status(200).json({ message: 'Login efetuado com sucesso' });
-  } catch (err) {
-    return res
-      .status(500)
-      .json({ message: 'Erro interno', error: err.message });
-  }
+		if (!validateBody(req.body, res)) return;
+
+		const user = await User.findOne({ where: { username } });
+
+		if (!user || user.password !== password) {
+			return res.status(401).json({ message: 'Usuário não existe ou senha inválida' });
+		}
+		const jwtConfig = {
+			expiresIn: '7d',
+			algorithm: 'HS256',
+		};
+
+		const token = jwt.sign({ data: user }, secret, jwtConfig);
+
+		return res.status(200).json({ token });
+	} catch (err) {
+		return res.status(500).json({ message: 'Erro interno', error: err.message });
+	}
 };
