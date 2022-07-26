4d81e80c68b4d74e59b33fd9649696d17fbf6ad4
AugustoMagalhaes <augustommg@gmail.com>
2022-07-26 16:25:14 -0300

    testa uso de enum com dias da semana.

--- back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/first-typescript-attempt/src/index.js
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/first-typescript-attempt/src/index.js
@@ -1,2 +1,16 @@
 var idade = 30;
 var nome = 'Jose';
+var daysOfWeek;
+(function (daysOfWeek) {
+    daysOfWeek[daysOfWeek["domingo"] = 1] = "domingo";
+    daysOfWeek[daysOfWeek["segunda"] = 2] = "segunda";
+    daysOfWeek[daysOfWeek["ter\u00E7a"] = 3] = "ter\u00E7a";
+    daysOfWeek[daysOfWeek["quarta"] = 4] = "quarta";
+    daysOfWeek[daysOfWeek["quinta"] = 5] = "quinta";
+    daysOfWeek[daysOfWeek["sexta"] = 6] = "sexta";
+    daysOfWeek[daysOfWeek["sabado"] = 7] = "sabado";
+})(daysOfWeek || (daysOfWeek = {}));
+console.log('segunda', daysOfWeek.segunda);
+console.log('dia de quarta', daysOfWeek[4]);
+console.log('numero de sabado', daysOfWeek['sabado']);
+console.log('dia de sabado', daysOfWeek[7]);
--- back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/first-typescript-attempt/src/index.ts
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/first-typescript-attempt/src/index.ts
@@ -1,3 +1,18 @@
 let idade = 30;
 
 const nome: string = 'Jose';
+
+enum daysOfWeek {
+  domingo = 1,
+  segunda,
+  terça,
+  quarta,
+  quinta,
+  sexta,
+  sabado,
+}
+
+console.log('segunda', daysOfWeek.segunda);
+console.log('dia de quarta', daysOfWeek[4]);
+console.log('numero de sabado', daysOfWeek['sabado']);
+console.log('dia de sabado', daysOfWeek[7]);
