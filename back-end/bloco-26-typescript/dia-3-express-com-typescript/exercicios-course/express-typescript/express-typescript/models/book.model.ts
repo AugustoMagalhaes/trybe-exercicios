ba50bfd8aaf46e7fd136e1038894f72b3bf0564c
AugustoMagalhaes <augustommg@gmail.com>
2022-07-29 18:01:03 -0300

    feat: implementa rota PUT, model, service e controllers respectivos.

--- back-end/bloco-26-typescript/dia-3-express-com-typescript/exercicios-course/express-typescript/express-typescript/models/book.model.ts
+++ back-end/bloco-26-typescript/dia-3-express-com-typescript/exercicios-course/express-typescript/express-typescript/models/book.model.ts
@@ -31,4 +31,12 @@ export default class BookModel {
     const [book] = rows as Book[];
     return book;
   }
+
+  public async update(id: number, book: Book) {
+    const { title, price, author, isbn } = book;
+    await this.connection.execute(
+      'UPDATE books SET title=?, price=?, author=?, isbn=? WHERE id=?',
+      [title, price, author, isbn, id],
+    );
+  }
 }
