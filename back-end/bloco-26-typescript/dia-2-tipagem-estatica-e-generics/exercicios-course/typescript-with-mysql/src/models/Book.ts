b5593ea98d9ca4aef06ec8fea335c23e12aee3e9
AugustoMagalhaes <augustommg@gmail.com>
2022-07-29 02:07:01 -0300

    feat: altera model Book para implementar interface Book.

--- back-end/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/exercicios-course/typescript-with-mysql/src/models/Book.ts
+++ back-end/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/exercicios-course/typescript-with-mysql/src/models/Book.ts
@@ -1,5 +1,13 @@
 import { Pool } from 'mysql2/promise';
 
+export interface Book {
+  id?: number;
+  title: string;
+  price: number;
+  author: string;
+  isbn: string;
+}
+
 export default class BookModel {
   private connection: Pool;
 
@@ -7,9 +15,9 @@ export default class BookModel {
     this.connection = connection;
   }
 
-  public async getAll() {
+  public async getAll(): Promise<Book[]> {
     const result = await this.connection.execute('SELECT * FROM books');
     const [rows] = result;
-    return rows;
+    return rows as Book[];
   }
 }
