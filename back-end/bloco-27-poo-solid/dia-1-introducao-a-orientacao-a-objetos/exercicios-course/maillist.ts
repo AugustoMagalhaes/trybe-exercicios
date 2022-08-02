ec2809dccbd60ec15c005f274c8bcd857dadbfdb
AugustoMagalhaes <augustommg@gmail.com>
2022-08-02 16:27:00 -0300

    feat: implementa classe MailList.

---
+++ back-end/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/exercicios-course/maillist.ts
@@ -0,0 +1,24 @@
+class MailList {
+  constructor(private mailList: Email[] = []) { }
+
+  get all(): Email[] { return this.mailList; }
+
+  getEmailsSentBy(mailAddress: string): Email[] {
+    return this.mailList.filter((mail) => mail.from === mailAddress);
+  }
+
+  getEmailsTo(mailAddress: string): Email[] {
+    return this.mailList.filter((mail) => mail.to === mailAddress);
+  }
+
+  getEmailsBySubject(searchString: string): Email[] {
+    return this.mailList
+      .filter((mail) => mail.subject.indexOf(searchString) !== -1);
+  }
+
+  addEmail(newMail: Email): void { this.mailList.push(newMail); }
+
+  removeEmail(mailToRemove: Email): void {
+    this.mailList = this.mailList.filter((mail) => mail !== mailToRemove);
+  }
+}
