7a45ac48e9d0f6409aabb3f953ead2355316de89
AugustoMagalhaes <augustommg@gmail.com>
2022-08-08 01:50:00 -0300

    refac: refatora as funçoes pra se adequar definitivamente ao principio de responsabilidade unica.

--- back-end/bloco-27-poo-solid/dia-4-solid-introducao-e-principios-s-o-e-d/exercicios-course/srp/src/index.ts
+++ back-end/bloco-27-poo-solid/dia-4-solid-introducao-e-principios-s-o-e-d/exercicios-course/srp/src/index.ts
@@ -9,33 +9,44 @@ type Discipline = {
 type Student = {
   name: string;
   disciplines: Discipline[];
+  school?: string;
 };
 
-const percentageGradesIntoLetters = ({ name: studentName, disciplines }: Student):
-  { name: string, disciplines: Discipline[]} => ({
-  name: studentName,
-  disciplines: disciplines.map(({ name, grade }) => {
-    let letterGrade;
+/* Apoio para a função `getGradeLetter` */
+const GRADE_DICT = {
+  numbers: [0.9, 0.8, 0.7, 0.6, 0.1],
+  letters: ['A', 'B', 'C', 'D', 'E'],
+};
 
-    if (grade >= 0.9) letterGrade = 'A';
-    else if (grade >= 0.8) letterGrade = 'B';
-    else if (grade >= 0.7) letterGrade = 'C';
-    else if (grade >= 0.6) letterGrade = 'D';
-    else if (grade >= 0.1) letterGrade = 'E';
-    else letterGrade = 'F';
+/* Função menor para remover o uso excessivo de `if{}else`s */
+const getGradeLetter = (gradeNumber: number): string => {
+  const gradeNumbers = GRADE_DICT.numbers;
+  const gradeLetters = GRADE_DICT.letters;
+  for (let i = 0; i < gradeNumbers.length; i += 1) {
+    if (gradeNumber >= gradeNumbers[i]) return gradeLetters[i];
+  }
+  return 'F';
+};
 
-    return { name, grade, letterGrade };
-  }) });
+/* Coletar notas */
+const getLetterGrades = (discipline: Discipline): Discipline => ({
+  ...discipline,
+  letterGrade: getGradeLetter(discipline.grade) });
 
+/* "Converter" */
+const percentageGradesIntoLetters = (student: Student): Student => ({
+  ...student,
+  disciplines: student.disciplines.map(getLetterGrades) });
+
+/* "Determinar" */
 const approvedStudents = ({ disciplines }: Student): boolean =>
-  disciplines.every(
-    ({ grade }) => grade > 0.7,
-  );
+  disciplines.every(({ grade }) => grade > 0.7);
 
-const updateApprovalData = ({ name: studentName, disciplines }: Student): void => {
-  console.log(`A pessoa com nome ${studentName} foi aprovada!`);
+/* "Atualizar" */
+const updateApprovalData = (student: Student): void => {
+  console.log(`A pessoa com nome ${student.name} foi aprovada!`);
 
-  disciplines.map(({ name, letterGrade }) =>
+  student.disciplines.forEach(({ name, letterGrade }) =>
     console.log(`${name}: ${letterGrade}`));
 };
 
@@ -51,4 +62,5 @@ export {
   approvedStudents,
   updateApprovalData,
   setApproved,
+  getLetterGrades,
 };
