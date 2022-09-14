022fe9a3e8f4fb1de5ffdfe83a416d5644584fc6
AugustoMagalhaes <augustommg@gmail.com>
2022-09-14 03:27:50 -0300

    chore: substitui erro generico por erro do ErrorTypes.

--- back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-2-mongoose-e-arquitetura-msccamada-service-e-controller/exercicios-course/guia-mongodb-com-nodejs-e-poo/src/models/MongoModel.ts
+++ back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-2-mongoose-e-arquitetura-msccamada-service-e-controller/exercicios-course/guia-mongodb-com-nodejs-e-poo/src/models/MongoModel.ts
@@ -1,18 +1,19 @@
 import { isValidObjectId, Model } from 'mongoose';
+import { ErrorTypes } from '../errors/catalog';
 import IModel from '../interfaces/IModel';
 
 abstract class MongoModel<T> implements IModel<T> {
-  protected _model:Model<T>;
+  protected _model: Model<T>;
 
-  constructor(model:Model<T>) {
+  constructor(model: Model<T>) {
     this._model = model;
   }
-  public async create(obj:T):Promise<T> {
+  public async create(obj: T): Promise<T> {
     return this._model.create({ ...obj });
   }
 
-  public async readOne(_id:string):Promise<T | null> {
-    if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
+  public async readOne(_id: string): Promise<T | null> {
+    if (!isValidObjectId(_id)) throw new Error(ErrorTypes.InvalidMongoId);
     return this._model.findOne({ _id });
   }
 }
