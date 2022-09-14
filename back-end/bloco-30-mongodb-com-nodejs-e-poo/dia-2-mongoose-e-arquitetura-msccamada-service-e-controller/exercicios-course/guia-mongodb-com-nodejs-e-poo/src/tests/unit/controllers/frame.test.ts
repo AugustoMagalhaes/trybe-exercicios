8396830f961b75e562549dfa12c38ae7af6ec701
AugustoMagalhaes <augustommg@gmail.com>
2022-09-14 03:55:53 -0300

    test: implementa testes do controller frame.

---
+++ back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-2-mongoose-e-arquitetura-msccamada-service-e-controller/exercicios-course/guia-mongodb-com-nodejs-e-poo/src/tests/unit/controllers/frame.test.ts
@@ -0,0 +1,49 @@
+import { expect } from 'chai';
+import { Request, Response } from 'express';
+import * as sinon from 'sinon';
+import FrameController from '../../../controllers/Frame';
+import FrameModel from '../../../models/Frame';
+import FrameService from '../../../services/Frame';
+import { frameMock, frameMockWithId } from '../../mocks/frameMock';
+
+describe('Frame Controller', () => {
+  const frameModel = new FrameModel();
+  const frameService = new FrameService(frameModel);
+  const frameController = new FrameController(frameService);
+
+  const req = {} as Request;
+
+  const res = {} as Response;
+
+  before(() => {
+    sinon.stub(frameService, 'create').resolves(frameMock);
+    sinon.stub(frameService, 'readOne').resolves(frameMock);
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+  });
+
+  after(() => {
+    sinon.restore();
+  });
+
+  describe('Create Frame', () => {
+    it('Success', async () => {
+      req.body = frameMock;
+      await frameController.create(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(frameMock)).to.be.true;
+    });
+  });
+
+  describe('ReadOne Frame', () => {
+    it('Success', async () => {
+      req.params = { id: frameMockWithId._id };
+      await frameController.readOne(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(frameMock)).to.be.true;
+    });
+  });
+});
